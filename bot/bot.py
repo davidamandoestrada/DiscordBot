27c03eaae1a73c33dcdf780d99c609a593d2bdad
David Estrada <davidamandoestrada@gmail.com>
2019-12-29 11:01:07 -0800

    Cleaned up how messages are handled in bot.py

--- bot/bot.py
+++ bot/bot.py
@@ -92,26 +92,18 @@ async def on_playback_command(ctx):
 
 
 async def _handle_private_message(message):
+    async def respond_to_private_message(response_message):
+        await message.author.create_dm()
+        await message.author.dm_channel.send(response_message)
+
     session = requests.Session()
     payload = {
         "content": message.content,
     }
     try:
         response = session.get(f"{chatbot}/message/", json=payload)
-        if response.status_code == 200:
-            response_message = response.json()["response_message"]
-            error = response.json()["error"]
-            if error is None:
-                if response_message:
-                    await message.author.create_dm()
-                    await message.author.dm_channel.send(response_message)
-            else:
-                response_message = f"An error has occurred while processing this message. Error: {error}"
-                await message.author.create_dm()
-                await message.author.dm_channel.send(response_message)
-        else:
-            await message.author.create_dm()
-            await message.author.dm_channel.send(response)
+        function_to_use_to_respond = respond_to_private_message
+        await _handle_message_response(response, message, function_to_use_to_respond)
     except Exception as exception:
         await message.author.create_dm()
         await message.author.dm_channel.send(f"ERROR: {exception}")
@@ -128,19 +120,26 @@ async def _handle_non_private_message(message):
 
     try:
         response = session.get(f"{processor}/message/", json=payload)
-        if response.status_code == 200:
-            response_message = response.json()["response_message"]
-            error = response.json()["error"]
-            if error is None:
-                if response_message:
-                    await message.channel.send(response_message)
-            else:
-                response_message = f"An error has occurred while processing this message. Error: {error}"
-                await message.channel.send(response_message)
-        else:
-            await message.channel.send(response)
+        function_to_use_to_respond = message.channel.send
+        await _handle_message_response(response, message, function_to_use_to_respond)
     except Exception as exception:
         await message.channel.send(f"ERROR: {exception}")
+
+
+async def _handle_message_response(response, message, function_to_use_to_respond):
+    if response.status_code == 200:
+        response_message = response.json()["response_message"]
+        error = response.json()["error"]
+        if error is None:
+            if response_message:
+                await function_to_use_to_respond(response_message)
+        else:
+            response_message = (
+                f"An error has occurred while processing this message. Error: {error}"
+            )
+            await function_to_use_to_respond(response_message)
+    else:
+        await function_to_use_to_respond(response)
 
 
 async def _send_avatar_image_to_discord(ctx):
