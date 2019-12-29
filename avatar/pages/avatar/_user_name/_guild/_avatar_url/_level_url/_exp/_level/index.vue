45ae3906a49df7951eda7d6530b7a297b3de7145
David Estrada <davidamandoestrada@gmail.com>
2019-12-28 16:28:43 -0800

    Updated the AvatarCard to use route to get more data that it needed (avatar_url, level_url, exp, level).

---
+++ avatar/pages/avatar/_user_name/_guild/_avatar_url/_level_url/_exp/_level/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <AvatarCard
+    :user_name="$route.params.user_name"
+    :guild="$route.params.guild"
+    :avatar_image_url="$route.params.avatar_url"
+    :level_image_url="$route.params.level_url"
+    :exp="$route.params.exp"
+    :level="$route.params.level"
+  />
+</template>
+
+<script>
+import AvatarCard from '~/components/AvatarCard.vue'
+
+export default {
+  components: {
+    AvatarCard
+  }
+}
+</script>
