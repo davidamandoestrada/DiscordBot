b5097f59cc167d401414ab686dc0aff47b381135
David Estrada <davidamandoestrada@gmail.com>
2019-12-28 16:27:54 -0800

    User data mapper will have avatar_url to deal with now as well

--- processor/data_mappers/user_data_mapper.py
+++ processor/data_mappers/user_data_mapper.py
@@ -10,18 +10,32 @@ queries.connect("postgresql://postgres@db/discordbot")
 
 
 class UserDataMapper:
-    def create(self, user_name: str):
-        queries.insert_user(user_name=user_name)
+    def create(self, user_name: str, avatar_url: str):
+        queries.insert_user(user_name=user_name, avatar_url=avatar_url)
+
+    def update(self, user):
+        queries.update_user(
+            id=user.id, user_name=user.user_name, avatar_url=user.avatar_url
+        )
 
     def find_users(self):
         results = queries.find_users()
         return [
-            User(id=result["id"], user_name=result["user_name"]) for result in results
+            User(
+                id=result["id"],
+                user_name=result["user_name"],
+                avatar_url=result["avatar_url"],
+            )
+            for result in results
         ]
 
     def find_user_by_user_name(self, user_name: str):
         result = queries.find_user_by_user_name(user_name=user_name)
         if result:
-            return User(id=result["id"], user_name=result["user_name"])
+            return User(
+                id=result["id"],
+                user_name=result["user_name"],
+                avatar_url=result["avatar_url"],
+            )
         else:
             raise UserNotFoundError
