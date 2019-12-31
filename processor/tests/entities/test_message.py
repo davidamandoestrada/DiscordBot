4121352160a015a8c99ba3edd6b480ae909ba086
David Estrada <davidamandoestrada@gmail.com>
2019-12-30 18:39:45 -0800

    More tests for entities

--- processor/tests/entities/test_message.py
+++ processor/tests/entities/test_message.py
@@ -1,5 +1,3 @@
-import pytest
-
 from data_mappers.registry import DataMapperRegistry
 from entities.message import Message, find_messages_by_user
 from entities.user import User
@@ -17,17 +15,19 @@ class TestMessage:
         messages = find_messages_by_user(user)
 
         assert len(messages) == 1
+
+    def test_message_fields_will_be_populated_correctly(self):
+        user = User(_id=1, avatar_url="google.com", user_name="test_user")
+        Message.create_new(author=user, content="test_content")
+        messages = find_messages_by_user(user)
+
         assert messages[0].author_id == user._id
         assert messages[0].content == "test_content"
 
-    def test_create_multiple_new_messages_and_find_it(self):
+    def test_create_multiple_new_messages_by_user_and_find_them(self):
         user = User(_id=1, avatar_url="google.com", user_name="test_user")
         Message.create_new(author=user, content="test_content")
         Message.create_new(author=user, content="test_content 2")
         messages = find_messages_by_user(user)
 
         assert len(messages) == 2
-        assert messages[0].author_id == user._id
-        assert messages[0].content == "test_content"
-        assert messages[1].author_id == user._id
-        assert messages[1].content == "test_content 2"
